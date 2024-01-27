ef106a35cf31609c4d19567134d6069a942e1da2
AbderrahmanBoulbaz <elbouatmani3@gmail.com>
2024-01-27 01:16:31 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,49 @@
+while True :
+#first_number
+   while True :
+     try:
+      first_number = float(input('entre le premié nomber :' ))
+      break
+     except ValueError :
+         print('please entere numinc variabl')
+# op input
+   while True :
+    try:
+     op =input('enter opération  :')
+     if op in ('+','/','*','-') :
+        break
+     else:
+         raise ValueError
+    except ValueError :
+     print('invalid op pleas entrer / * + -')
+# second number
+   while True :
+    try:
+     second_number = float(input('enter le dexiéme nomber :'))
+     if second_number == 0 and op == '/' :
+         raise ZeroDivisionError
+     break
+    except ValueError:
+       print('please entere numinc variabl')
+    except ZeroDivisionError:
+        print('cannot divide by zero , please enter the new number')
+# print the reslt
+   if op == '+':
+        résulta = first_number + second_number
+   elif op == '−':
+       résulta = first_number -second_number
+   elif op == '/':
+        résulta = first_number / second_number
+   elif op == '*':
+        résulta = first_number * second_number
+   else:
+        résulta = ('error')
+   print(résulta)
+   if résulta != None :
+     print('result is ',résulta)
+   else:
+      print('error')
+   X = input(' do you want to perform another op (y/n) :')
+   if X == 'n':
+        break
+print('progame exist')
